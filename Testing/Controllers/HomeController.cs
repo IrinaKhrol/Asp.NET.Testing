ecb904768f521f09cf86f60ee357186cefde4c7c
Irina Khrol <Irina@gmail.com>
2024-06-11 10:41:33 +0300

    Add project files.

---
+++ Testing/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Testing.Models.Product;
+using Testing.Models;
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IDataSource dataSource = new ProductDataSource();
+        public ViewResult Index()
+        {
+            return View(dataSource.Products);
+        }
+    }
+}
